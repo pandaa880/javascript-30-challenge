e7988ee3fdc75a75410e52faf0b0b78c3df7ec7e
Prashant Chaudhari <prashantchaudhari881@gmail.com>
2020-06-03 15:33:09 +0530

    day-1 create drum kit using js

---
+++ 01 - JavaScript Drum Kit/script.js
@@ -0,0 +1,34 @@
+window.addEventListener("keydown", function (e) {
+  // select an audio with associated keyCode
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+  // if there is no audio then stop the function
+  if (!audio) {
+    return;
+  }
+
+  // rewind it to start
+  audio.currentTime = 0;
+  audio.play();
+
+  // add class for animation
+  key.classList.add("playing");
+});
+
+// remove transition
+function removeTransition(e) {
+  if (e.propertyName !== "transform") {
+    return;
+  }
+
+  // here `this` is the key
+  this.classList.remove("playing");
+}
+
+// select all keys to detect when animation ends - when animation ends we remove the .playing class from the element
+const keys = document.querySelectorAll(".key");
+// add an event listener to each key
+keys.forEach((key) => {
+  key.addEventListener("transitionend", removeTransition);
+});
